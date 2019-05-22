ec9217d1c1154fae5768784f2277d9b6abb1b817
Murat SAÇ <muratsac@mail.com>
2019-05-22 21:19:47 +0300

    xml writer updated

--- src/Nestpay/Request/PurchaseRequest.php
+++ src/Nestpay/Request/PurchaseRequest.php
@@ -7,50 +7,6 @@ use Payconn\Nestpay\Response\PurchaseResponse;
 
 class PurchaseRequest extends NestpayRequest
 {
-    protected $postData = '<?xml version="1.0" encoding="ISO-8859-9"?>
-        <CC5Request>
-            <Name></Name>
-            <Password></Password>
-            <ClientId></ClientId>
-            <IPAddress></IPAddress>
-            <Email></Email>
-            <Mode></Mode>
-            <OrderId></OrderId>
-            <GroupId></GroupId>
-            <TransId></TransId>
-            <UserId></UserId>
-            <Type></Type>
-            <Number></Number>
-            <Expires></Expires>
-            <Cvv2Val></Cvv2Val>
-            <Total></Total>
-            <Currency></Currency>
-            <Taksit></Taksit>
-            <BillTo>
-                <Name></Name>
-                <Street1></Street1>
-                <Street2></Street2>
-                <Street3></Street3>
-                <City></City>
-                <StateProv></StateProv>
-                <PostalCode></PostalCode>
-                <Country></Country>
-                <Company></Company>
-                <TelVoice></TelVoice>
-            </BillTo>
-            <ShipTo>
-                <Name></Name>
-                <Street1></Street1>
-                <Street2></Street2>
-                <Street3></Street3>
-                <City></City>
-                <StateProv></StateProv>
-                <PostalCode></PostalCode>
-                <Country></Country>
-            </ShipTo>
-            <Extra></Extra>
-        </CC5Request>';
-
     public function getInstallment(): int
     {
         return $this->parameters->get('installment');
@@ -68,21 +24,23 @@ class PurchaseRequest extends NestpayRequest
 
     public function send(): ResponseInterface
     {
-        $body = @simplexml_load_string($this->postData);
-        $body->Type = 'Auth';
-        $body->Name = $this->getToken()->getUsername();
-        $body->Password = $this->getToken()->getPassword();
-        $body->ClientId = $this->getToken()->getClientId();
-        $body->IPAddress = $this->getIpAddress();
-        $body->Mode = $this->getMode();
-        $body->Total = $this->getAmount();
-        $body->Taksit = $this->getInstallment();
-        $body->Currency = $this->getCurrency();
+        $body = new \SimpleXMLElement('<?xml version="1.0" encoding="ISO-8859-9"?><CC5Request></CC5Request>');
+        $body->addChild('Type', 'Auth');
+        $body->addChild('Name', $this->getToken()->getUsername());
+        $body->addChild('Password', $this->getToken()->getPassword());
+        $body->addChild('ClientId', $this->getToken()->getClientId());
+        $body->addChild('IPAddress', $this->getIpAddress());
+        $body->addChild('Mode', $this->getMode());
+        $body->addChild('Total', $this->getAmount());
+        $body->addChild('Taksit', $this->getInstallment());
+        $body->addChild('Currency', $this->getCurrency());
+
         if ($creditCard = $this->getCreditCard()) {
-            $body->Number = $creditCard->getNumber();
-            $body->Expires = $creditCard->getExpireMonth().'/'.$creditCard->getExpireYear();
-            $body->Cvv2Val = $creditCard->getCvv();
+            $body->addChild('Number', $creditCard->getNumber());
+            $body->addChild('Expires', $creditCard->getExpireMonth().'/'.$creditCard->getExpireYear());
+            $body->addChild('Cvv2Val', $creditCard->getCvv());
         }
+
         $response = $this->getHttpClient()->request('POST', $this->getBaseUrl(), [
             'body' => $body->asXML(),
         ]);
