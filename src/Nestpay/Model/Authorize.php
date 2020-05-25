f0197bd591c62040f7d09d95abb4a7f9307d6c66
Ahmet Kaba <ahmetkaba007@gmail.com>
2020-05-25 11:53:33 +0300

    cs fix

--- src/Nestpay/Model/Authorize.php
+++ src/Nestpay/Model/Authorize.php
@@ -8,8 +8,8 @@ use Payconn\Common\Traits\Amount;
 use Payconn\Common\Traits\CreditCard;
 use Payconn\Common\Traits\Currency;
 use Payconn\Common\Traits\Installment;
-use Payconn\Common\Traits\ReturnUrl;
 use Payconn\Common\Traits\OrderId;
+use Payconn\Common\Traits\ReturnUrl;
 
 class Authorize extends AbstractModel implements AuthorizeInterface
 {
